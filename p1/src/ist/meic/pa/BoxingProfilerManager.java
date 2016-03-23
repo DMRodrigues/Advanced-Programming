0a8c960c297246edbb02cb0bf73c573a08c975a4
MartaNascimento <marta.nascimento02@gmail.com>
2016-03-23 16:10:35 +0000

    [P1] tests passed

--- p1/src/ist/meic/pa/BoxingProfilerManager.java
+++ p1/src/ist/meic/pa/BoxingProfilerManager.java
@@ -8,21 +8,28 @@ import java.util.TreeMap;
 import javassist.CannotCompileException;
 import javassist.CtClass;
 import javassist.CtMethod;
+import javassist.CtPrimitiveType;
+import javassist.NotFoundException;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
 public class BoxingProfilerManager {
 
 	private CtClass ctClass;
-
-	static final String TEMPLATE = "{ " + 
-			"ist.meic.pa.BoxingProfilerManager.inc(\"%s\", \"%s\", \"%s\");"
-			+ "$_ = $proceed($$); " + "}";
-
-	static Map<String, Map<String, Map<String, Integer>>> resultMap = new TreeMap<String, Map<String, Map<String, Integer>>>();
-
 	private String[] args;
 	private Class<? extends String[]> argsClass;
+
+	private static final String BOXED = "boxed";
+	private static final String UNBOXED = "unboxed";
+
+	private static final String BOX_TEMPLATE = "{ "
+			+ "ist.meic.pa.BoxingProfilerManager.inc(\"%s\", \"%s\", \"%s\");"
+			+ "$_ = $proceed($$); " + "}";
+	private static final String UNBOX_TEMPLATE = "{ "
+			+ "ist.meic.pa.BoxingProfilerManager.inc(\"%s\", \"%s\", \"%s\");"
+			+ "$_ = $proceed(); " + "}";
+
+	private static Map<String, Map<String, Map<String, Integer>>> resultMap = new TreeMap<String, Map<String, Map<String, Integer>>>();
 
 	public BoxingProfilerManager(CtClass ctClass, String[] args) {
 		this.ctClass = ctClass;
@@ -32,13 +39,13 @@ public class BoxingProfilerManager {
 		this.argsClass = args.getClass();
 	}
 
-	public void profile(){
+	public void profile() {
 		for (CtMethod ctMethod : ctClass.getDeclaredMethods()) {
 
 			if (!resultMap.containsKey(ctMethod.getLongName())) {
 				resultMap.put(ctMethod.getLongName(), new TreeMap<String, Map<String, Integer>>());
 			}
-			
+
 			try {
 				ctMethod.instrument(new ExprEditor() {
 
@@ -54,20 +61,28 @@ public class BoxingProfilerManager {
 								m.put(className, new TreeMap<String, Integer>());
 							}
 							Map<String, Integer> mm = m.get(className);
-							mm.put("boxed", 0);
-							mc.replace(String.format(TEMPLATE, methodName, className, "boxed"));
+							mm.put(BOXED, 0);
+							mc.replace(String.format(BOX_TEMPLATE, methodName, className, BOXED));
 
 						} else if (mc.getMethodName().equals("intValue") || mc.getMethodName().equals("longValue")
 								|| mc.getMethodName().equals("doubleValue") || mc.getMethodName().equals("floatValue")
 								|| mc.getMethodName().equals("booleanValue") || mc.getMethodName().equals("byteValue")
 								|| mc.getMethodName().equals("charValue") || mc.getMethodName().equals("shortValue")) {
+							try {
+								String wrapperType = ((CtPrimitiveType) mc.getMethod().getReturnType())
+										.getWrapperName();
+								if (wrapperType.equals(className)) {
 
-							if (!m.containsKey(className)) {
-								m.put(className, new TreeMap<String, Integer>());
+									if (!m.containsKey(className)) {
+										m.put(className, new TreeMap<String, Integer>());
+									}
+									Map<String, Integer> mm = m.get(className);
+									mm.put(UNBOXED, 0);
+									mc.replace(String.format(UNBOX_TEMPLATE, methodName, className, UNBOXED));
+								}
+							} catch (NotFoundException e) {
+								System.err.println(e);
 							}
-							Map<String, Integer> mm = m.get(className);
-							mm.put("unboxed", 0);
-							mc.replace(String.format(TEMPLATE, methodName, className, "unboxed"));
 						}
 
 					}
@@ -79,12 +94,13 @@ public class BoxingProfilerManager {
 		}
 
 		try {
-		
+
 			Class<?> rtClass = ctClass.toClass();
 			Method main = rtClass.getMethod("main", argsClass);
 			main.invoke(null, new Object[] { args });
-			
-		} catch (CannotCompileException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+
+		} catch (CannotCompileException | NoSuchMethodException | SecurityException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException e) {
 			System.err.println(e);
 		}
 	}
@@ -100,8 +116,9 @@ public class BoxingProfilerManager {
 		for (String methodName : resultMap.keySet()) {
 			for (String className : resultMap.get(methodName).keySet()) {
 				for (String operation : resultMap.get(methodName).get(className).keySet()) {
-					Integer mci = resultMap.get(methodName).get(className).get(operation);
-					System.err.println(methodName + " " + operation + " " + mci + " " + className);
+					Integer counter = resultMap.get(methodName).get(className).get(operation);
+					if (counter > 0)
+						System.err.println(methodName + " " + operation + " " + counter + " " + className);
 				}
 			}
 		}
